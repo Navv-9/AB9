368e58c10ad0b3260334d156a2643039a1ebdd04
ns <navya.sharmaa16@gmail.com>
2019-10-18 16:46:40 +0530

    database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,50 @@
-var ball;
+var Hyball;
+var database, position;
 
 function setup(){
+    database= firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    Hyball = createSprite(250,250,10,10);
+    Hyball.shapeColor = "red";
+    var Hyballposition= database.ref('ball/position');
+    Hyballposition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
+
+if (position!== undefined){
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
+}}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+    'x':position.x + x,
+    'y':position.y + y})
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+
+function readPosition(data){
+position= data.val();
+Hyball.x= position.x;
+Hyball.y = position.y;
+}
+
+
+
+
+function showError(){
+    console.log("error in writing to the database");
 }
